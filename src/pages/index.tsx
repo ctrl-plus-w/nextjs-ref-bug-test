6e6c925a4877b177a6b2f493e5eb8992b2207b50
Lukas Laudrain <lukas.ldrn@gmail.com>
2023-03-09 11:04:19 +0100

    Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,61 @@
 import clsx from 'clsx';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const Content = ({ className}: { className: string }) => {
 	
 	const [webConsole, setWebConsole] = useState<string[]>([])
 
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
 	const log = (...messages: string[]) => {
 		setWebConsole(wc => [...wc, ...messages]);
 	}
+
+	useEffect(() => {
+		if (!canvasRef.current) return; 
+
+		const canvas = canvasRef.current;
+		const ctx = canvas.getContext("2d");
+
+		let frame: number;
+		let i = 0;
+
+		const render = () => {
+			if (ctx) {	
+				ctx.fillStyle = "#ffff00";
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+				ctx.fillRect(0, 0, canvas.width, i);
+
+				i++;
+				if (i > canvas.height) i = 0;
+			}
+
+		
+			frame = window.requestAnimationFrame(render);
+		}
+
+		frame = window.requestAnimationFrame(render);
+	
+		return () => {
+			if(frame) window.cancelAnimationFrame(frame);
+		}
+	}, [canvasRef]);
 
 	const ref = useCallback((node: HTMLDivElement) => {
 		if (!node) return;
 			
 		const { width, height } = node.getBoundingClientRect();
 		log(`Current size : (${width}, ${height})`);
+
+		const canvas = canvasRef.current;
+		if(canvas !== null){
+			canvas.width = width;
+			canvas.height = height;
+
+			canvas.style.width = `${width}px`;
+			canvas.style.height = `${height}px`;
+		}
 	}, []);
 
 	return <>
@@ -22,7 +64,7 @@ const Content = ({ className}: { className: string }) => {
 		</div>
 
 		<div className={clsx(["bg-purple-500", className])} ref={ref}>
-			<canvas></canvas>
+			<canvas ref={canvasRef}></canvas>
 		</div>
 	</>
 
